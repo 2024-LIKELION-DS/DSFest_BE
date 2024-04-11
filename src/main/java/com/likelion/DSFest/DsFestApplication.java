9af96c8f8e99ff061cd731df90d4b486ff0d1cb7
jiinkyung <jinkyung010310@gmail.com>
2024-04-12 00:05:41 +0900

    [INIT] 프로젝트 세팅

---
+++ src/main/java/com/likelion/DSFest/DsFestApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.DSFest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsFestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/DSFest/DsFestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.DSFest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsFestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
