760c5f6ecaefe4f11e1de6fde9e036026dda6aa9
jiinkyung <jinkyung010310@gmail.com>
2024-04-12 00:29:09 +0900

    [CHORE] Swagger 세팅

---
+++ src/main/java/com/likelion/DSFest/global/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.likelion.DSFest.global.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("2024 근화제 웹페이지 API")
+                        .description("2024 근화제 '찬란' 웹사이트 API 명세서")
+                        .version("1.0.0"));
+    }
+}
