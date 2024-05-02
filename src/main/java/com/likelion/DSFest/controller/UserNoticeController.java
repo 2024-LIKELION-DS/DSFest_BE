eb5c5688bd9ee1db70dfb733f339c9ce9bc30f43
7beunseo <kimes0403@gmail.com>
2024-05-03 00:32:55 +0900

    [FEAT] 사용자용 목록 조회 API 작성

---
+++ src/main/java/com/likelion/DSFest/controller/UserNoticeController.java
@@ -0,0 +1,42 @@
+package com.likelion.DSFest.controller;
+
+import com.likelion.DSFest.dto.ResponseDTO;
+import com.likelion.DSFest.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserNoticeController {
+    @Autowired
+    private NoticeService noticeService;
+
+    @GetMapping("read/all")
+    public ResponseEntity<ResponseDTO> readAll() {
+        try {
+            ResponseDTO response = noticeService.readAll();
+            return ResponseEntity.ok().body(response);
+
+        } catch (Exception e) {
+            ResponseDTO<Object> response = ResponseDTO.builder().message(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(response); //오류 메시지 확인할 수 있도록
+        }
+
+    }
+
+    @GetMapping("read/{id}")
+    public ResponseEntity<ResponseDTO> readOne(@PathVariable Integer id) {
+        try {
+            ResponseDTO response = noticeService.readOne(id);
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            ResponseDTO<Object> response = ResponseDTO.builder().message(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(response); //오류 메시지 확인할 수 있도록
+        }
+
+    }
+}
