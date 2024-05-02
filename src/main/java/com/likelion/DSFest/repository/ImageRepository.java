5241cb0fd5bab3e4779b9bd6b93c0dc24ed2c3fc
이규민 <90364652+20210815@users.noreply.github.com>
2024-05-02 21:40:52 +0900

    [FEAT] 이미지 조회 기능 구현 #9

--- src/main/java/com/likelion/DSFest/repository/ImageRepository.java
+++ src/main/java/com/likelion/DSFest/repository/ImageRepository.java
@@ -4,6 +4,9 @@ import com.likelion.DSFest.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+    public List<Image> findByNotice_NoticeId(Integer id);
 }
