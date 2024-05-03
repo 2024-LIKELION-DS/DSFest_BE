dbf497d160a96061f6a3aacc1108e042a6ead5c5
gyurili <inglifestora@naver.com>
2024-05-04 01:02:11 +0900

    [REFACTOR] 후기 작성 기능 2차 구현 #4

--- src/main/java/com/likelion/DSFest/repository/ReviewRepository.java
+++ src/main/java/com/likelion/DSFest/repository/ReviewRepository.java
@@ -4,11 +4,8 @@ import com.likelion.DSFest.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Override
-    List<Review> findAll();
+
 }
