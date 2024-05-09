7717ab496809f5b7963bdda512d549f2c6edfd8f
jiinkyung <jinkyung010310@gmail.com>
2024-05-10 03:01:34 +0900

    [CHORE] 패키지 구조 변경 #43

--- src/main/java/com/likelion/DSFest/aws/s3/AmazonS3Manager.java
+++ src/main/java/com/likelion/DSFest/aws/s3/AmazonS3Manager.java
@@ -1,19 +1,12 @@
 package com.likelion.DSFest.aws.s3;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.likelion.DSFest.global.config.AmazonConfig;
-import com.likelion.DSFest.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
--- src/main/java/com/likelion/DSFest/config/SwaggerConfig.java
+++ src/main/java/com/likelion/DSFest/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.likelion.DSFest.global.config;
+package com.likelion.DSFest.config;
 
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
