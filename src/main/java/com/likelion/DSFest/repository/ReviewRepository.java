3b7fc5e8ea84662c1b26b7f8adeeaf68dd745644
gyurili <inglifestora@naver.com>
2024-05-11 01:52:13 +0900

    [FEAT] 후기 최신순 정렬 추가

--- src/main/java/com/likelion/DSFest/repository/ReviewRepository.java
+++ src/main/java/com/likelion/DSFest/repository/ReviewRepository.java
@@ -3,9 +3,11 @@ package com.likelion.DSFest.repository;
 import com.likelion.DSFest.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Sort;
+import java.util.List;
 
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+    List<Review> findAll(Sort sort);
 }
