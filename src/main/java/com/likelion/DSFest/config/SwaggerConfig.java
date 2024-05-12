4522ab9ee243cde32beaa0a2fbdfdaa9ad1c94c2
jiinkyung <jinkyung010310@gmail.com>
2024-05-12 22:35:14 +0900

    [CHORE] Swagger에 https url 추가 #55

--- src/main/java/com/likelion/DSFest/config/SwaggerConfig.java
+++ src/main/java/com/likelion/DSFest/config/SwaggerConfig.java
@@ -2,17 +2,30 @@ package com.likelion.DSFest.config;
 
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("2024 근화제 웹페이지 API")
+                .description("2024 근화제 '찬란' 웹사이트 API 명세서")
+                .version("1.0.0");
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+
+        Server testServer = new Server();
+        testServer.setUrl("https://likelion-ds.site"); // https에 접근 가능하게 설정
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("2024 근화제 웹페이지 API")
-                        .description("2024 근화제 '찬란' 웹사이트 API 명세서")
-                        .version("1.0.0"));
+                .info(info)
+                .servers(List.of(localServer, testServer));
+
     }
 }
